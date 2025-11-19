4d106c11d527d252b87f7b2758bf9292b7bc144e
pushpak261 <pushpakrajendra@gmail.com>
2025-11-19 21:01:54 +0530

    latest changes made in api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export async function getAllLinks() {
   const res = await fetch(`${BASE_URL}/api/links`);
